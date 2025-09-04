85150f982fc009179234a0780d9aec776457081e
Kostas9093 <159778628+Kostas9093@users.noreply.github.com>
2025-09-04 13:43:25 +0100

    g

--- src/recipe/NutritionDB.js
+++ src/recipe/NutritionDB.js
@@ -3,10 +3,14 @@ export const NUTRITION_DB = {
   "brown rice": {  unit: "g", calories: 354, protein: 7.9, carbs: 77, fat: 2.8 },
   "broccoli": {  unit: "g", calories: 34, protein: 2.8, carbs: 7, fat: 0.4 },
   "banana": {  unit: "item", calories: 89, protein: 1.1, carbs: 23, fat: 0.3 },
-  "greek yogurt": {  unit: "g", calories: 54, protein: 10, carbs: 3.5, fat: 0 },
+  "yogurt Total 0%": {  unit: "g", calories: 54, protein: 10, carbs: 3.5, fat: 0 },
   "cheddar cheese": {  unit: "g", calories: 403, protein: 25, carbs: 1.3, fat: 33 },
   "oats": {  unit: "g", calories: 389, protein: 11.5, carbs: 66.3, fat: 6.9 },
   "spinach": {  unit: "g", calories: 23, protein: 2.9, carbs: 3.6, fat: 0.4 },
   "apple": { unit: "item", calories: 52, protein: 0.3, carbs: 14, fat: 0.2 },
-  "egg": {  unit: "item", calories: 72, protein: 6.3, carbs: 0.3, fat: 4.8 }
+  "egg": {  unit: "item", calories: 72, protein: 6.3, carbs: 0.3, fat: 4.8 },
+  "wrap": { unit: "item", calories: 170, protein: 10.4, carbs: 27.7, fat: 3.0 },
+  "egg whites": { unit: "g", calories: 43, protein: 10.8, carbs: 0, fat: 0 },
+  "potatos boiled": { unit: "g", calories: 72, protein: 1.8, carbs: 17, fat: 0.1 },
+  "olive oil": { unit: "g", calories: 884, protein: 0, carbs: 0, fat: 98 }
 }
